ead3e14e92e9312771dc86489491ceb8b6860552
Khiem <buithen.khiem2717@gmail.com>
2015-07-21 23:16:03 +0700

    update code

--- MikiEditorUI/ViewModel/ShellViewModel.cs
+++ MikiEditorUI/ViewModel/ShellViewModel.cs
@@ -10,9 +10,23 @@
             OnActive();
         }
 
-        private BindableCollection<Chapter> chapters;
+        private Chapter currentChapter;
 
-        private BindableCollection<Page> pages;
+        public Chapter CurrentChapter
+        {
+            get
+            {
+                return currentChapter;
+            }
+
+            set
+            {
+                currentChapter = value;
+                this.NotifyOfPropertyChange(() => this.CurrentChapter);
+            }
+        }
+
+        private BindableCollection<Chapter> chapters;
 
         public BindableCollection<Chapter> Chapters
         {
@@ -28,31 +42,63 @@
             }
         }
 
-        public BindableCollection<Page> Pages
-        {
-            get
-            {
-                return pages;
-            }
+        //private BindableCollection<Page> pages;
 
-            set
-            {
-                pages = value;
-                this.NotifyOfPropertyChange(() => this.Pages);
-            }
-        }
+        //public BindableCollection<Page> Pages
+        //{
+        //    get
+        //    {
+        //        return pages;
+        //    }
+
+        //    set
+        //    {
+        //        pages = value;
+        //        this.NotifyOfPropertyChange(() => this.Pages);
+        //    }
+        //}
 
         private void OnActive()
         {
             if (Chapters == null)
             {
                 Chapters = new BindableCollection<Chapter>();
-            }
+                var chapter1 = new Chapter
+                {
+                    Title = "Chapter1",
+                    Pages =
+                        new BindableCollection<Page>
+                        {
+                            new Page() {ImgPath = "Image/blankimage.jpeg"},
+                            new Page() {ImgPath = "Image/blankimage.jpeg"},
+                            new Page() {ImgPath = "Image/blankimage.jpeg"}
+                        }
+                };
 
-            if (Pages == null)
-            {
-                Pages = new BindableCollection<Page>();
+                var chapter2 = new Chapter
+                {
+                    Title = "Chapter2",
+                    Pages =
+                        new BindableCollection<Page>
+                        {
+                            new Page() {ImgPath = "Image/blankimage.jpeg"},
+                            new Page() {ImgPath = "Image/blankimage.jpeg"}
+                        }
+                };
+
+                Chapters.Add(chapter1);
+                Chapters.Add(chapter2);
             }
+        }
+
+        public void AddNewPage()
+        {
+            CurrentChapter.Pages.Add(new Page() { ImgPath = "Image/blankimage.jpeg" });
+        }
+
+        public void ChapterChange()
+        {
+           
         }
     }
 }
