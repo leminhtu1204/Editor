54f93b247bc98236e1e5b28d0a04023d245a19ce
leminhtu1204 <tu.leminh@harveynash.vn>
2015-07-22 13:57:52 +0700

    tu

--- MikiEditor/MainWindow.xaml.cs
+++ MikiEditor/MainWindow.xaml.cs
@@ -2,14 +2,22 @@
 using System.IO;
 using System.Windows;
 using MikiEditor.BusinessObject;
-
+using ICSharpCode.SharpZipLib;      
 namespace MikiEditor
 {
+    using System;
+    using System.IO.Compression;
+
+    using ICSharpCode.SharpZipLib.Core;
+    using ICSharpCode.SharpZipLib.Zip;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        delegate void ProgressDelegate(string sMessage);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -56,6 +64,22 @@ namespace MikiEditor
 
             ConvertJson(comic, path); // create meta data
 
+            if (!Directory.Exists(@"C:\zip"))
+            {
+                Directory.CreateDirectory(@"C:\zip");
+            }
+
+            FileStream fsOut = File.Create(@"C:\zip" + @"\manga.zip");
+
+            ZipOutputStream zipStream = new ZipOutputStream(fsOut);
+
+            CompressFolder(path, zipStream, 1);
+
+            zipStream.IsStreamOwner = true;
+
+            zipStream.Close();
+            
+
         }
 
         private void ConvertJson(Comic comic, string path)
@@ -74,7 +98,51 @@ namespace MikiEditor
 
         public void CopyFiles(string sourcePath, string destinationPath)
         {
-            System.IO.File.Copy(sourcePath, destinationPath);
+            System.IO.File.Copy(sourcePath, destinationPath, true);
+        }
+
+        private void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+        {
+
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string filename in files)
+            {
+
+                FileInfo fi = new FileInfo(filename);
+
+                string entryName = filename.Substring(folderOffset); // Makes the name in zip based on the folder
+                entryName = ZipEntry.CleanName(entryName); // Removes drive from name and fixes slash direction
+                ZipEntry newEntry = new ZipEntry(entryName);
+                newEntry.DateTime = fi.LastWriteTime; // Note the zip format stores 2 second granularity
+
+                // Specifying the AESKeySize triggers AES encryption. Allowable values are 0 (off), 128 or 256.
+                // A password on the ZipOutputStream is required if using AES.
+                //   newEntry.AESKeySize = 256;
+
+                // To permit the zip to be unpacked by built-in extractor in WinXP and Server2003, WinZip 8, Java, and other older code,
+                // you need to do one of the following: Specify UseZip64.Off, or set the Size.
+                // If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, you do not need either,
+                // but the zip will be in Zip64 format which not all utilities can understand.
+                //   zipStream.UseZip64 = UseZip64.Off;
+                newEntry.Size = fi.Length;
+
+                zipStream.PutNextEntry(newEntry);
+
+                // Zip the file in buffered chunks
+                // the "using" will close the stream even if an exception occurs
+                byte[] buffer = new byte[4096];
+                using (FileStream streamReader = File.OpenRead(filename))
+                {
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+            string[] folders = Directory.GetDirectories(path);
+            foreach (string folder in folders)
+            {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
         }
 
     }
