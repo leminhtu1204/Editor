e1334ad819145d7e116405ecfccf1ec1397cd6cc
leminhtu1204 <tu.leminh@harveynash.vn>
2015-07-23 21:08:41 +0700

    Tu

---
+++ MikiEditorUI/ViewModel/NewComicModel.cs
@@ -0,0 +1,46 @@
+﻿namespace MikiEditorUI.ViewModel
+{
+    using System.Windows.Forms;
+
+    using Caliburn.Micro;
+
+    using MikiEditorUI.BusinessObject;
+
+    public class NewComicModel : PropertyChangedBase
+    {
+        private Comic comic;
+
+        public NewComicModel(Comic _comic)
+        {
+            this.comic = _comic;
+        }
+        public Comic Comic
+        {
+            get
+            {
+                return comic;
+            }
+            set
+            {
+                comic = value;
+                this.NotifyOfPropertyChange(() => this.Comic);
+            }
+        }
+
+        public void SaveComic()
+        {
+            var result = comic;
+        }
+
+        public void OpenSaveWorkSpace()
+        {
+            var folderDialog = new FolderBrowserDialog { SelectedPath = "C:\\" };
+            DialogResult result = folderDialog.ShowDialog();
+            if (result.ToString() == "OK")
+                comic.WorkSpace = folderDialog.SelectedPath;
+
+        }
+
+
+    }
+}
